dfbd4b7b078c5030a93afe411c808ca0698bf4bf
GeronimoFrancoKumulus <62804446+GeronimoFrancoKumulus@users.noreply.github.com>
2020-05-03 08:04:57 +0100

    New Upload

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
+const restify = require('restify');
 const express = require('express');
 const routes = require('./routes');
+
+const server = restify.createServer({name : "Node 1"})
+
+function hell(req, res, next) {
+    const mensagem = {texto:"hello world"};
+    res.json(mensagem);
+    next;
+}
+
+server.get('/hell', hell);
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333, function () {
+    console.log('escutando', server.name);
+});
+//app.listen(3333);
